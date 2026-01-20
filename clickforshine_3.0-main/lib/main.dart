5a91d407c3c9ae3648fc833352ebc3f3a8182c08
jhmspro1-code <jhms.pro1@gmail.com>
2026-01-20 17:12:18 -0300

    Update main.dart

--- clickforshine_3.0-main/lib/main.dart
+++ clickforshine_3.0-main/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-//import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
 import 'presentation/pages/home_page.dart';
 
